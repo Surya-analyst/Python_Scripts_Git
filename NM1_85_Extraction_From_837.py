5591324eb4b44aa3a50e238c19a0d3e98ee886cd
Surya Data Analyst <78232604+Surya-analyst@users.noreply.github.com>
2025-03-05 09:09:42 +0530

    NM1 85 Segment Extraction from EDI 837
    This script will extract NM1*85 segment from EDI 837 file

---
+++ NM1_85_Extraction_From_837.py
@@ -0,0 +1,33 @@
+import os
+from tkinter import Tk, filedialog, simpledialog, messagebox
+import re
+
+root = Tk()  # pointing root to Tk() to use it as Tk() in program.
+root.withdraw()  # Hides small tkinter window.
+root.attributes('-topmost', True)  # Opened windows will be active. above all windows despite of selection.
+cwd = filedialog.askdirectory().replace('/', '\\')  # Returns opened path as str
+
+if cwd != "":
+    path = os.path.join(cwd, "extracted_file").replace('/', '\\')
+    os.chdir(cwd)
+    try:
+        os.stat(path)
+    except:
+        os.mkdir(path)
+    for filename in os.listdir(cwd):
+        if os.path.isfile(os.path.join(cwd, filename)):
+            with open(filename, 'r') as s:
+                with open(os.path.join(path, filename), 'w') as f:
+                    for line in s:
+                        if line.startswith('NM1*85*'):
+                            f.write(line.rstrip())
+                            f.write("\n")
+    messagebox.showinfo('Info', 'Process completed!')
+
+else:
+    messagebox.showinfo('Info', 'User Terminated!')
+
+
+
+
+
